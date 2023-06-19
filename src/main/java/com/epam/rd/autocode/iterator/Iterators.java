3e9de2ce6f9f3a61aba991102ee231552912a451
tornikee <tornikexoxiashvili@gmail.com>
2023-06-19 12:08:44 +0400

    change

--- src/main/java/com/epam/rd/autocode/iterator/Iterators.java
+++ src/main/java/com/epam/rd/autocode/iterator/Iterators.java
@@ -2,24 +2,127 @@ package com.epam.rd.autocode.iterator;
 
 import java.util.Iterator;
 
-class Iterators {
 
-    public static Iterator<Integer> intArrayTwoTimesIterator(int[] array){
-        throw new UnsupportedOperationException();
+public class Iterators {
+
+    public static Iterator<Integer> intArrayTwoTimesIterator(int[] array) {
+        return new Iterator<Integer>() {
+            private int index = 0;
+            private int count = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new java.util.NoSuchElementException();
+                }
+
+                int element = array[index];
+                count++;
+
+                if (count == 2) {
+                    count = 0;
+                    index++;
+                }
+
+                return element;
+            }
+        };
     }
 
     public static Iterator<Integer> intArrayThreeTimesIterator(int[] array) {
-        throw new UnsupportedOperationException();
+        return new Iterator<Integer>() {
+            private int index = 0;
+            private int count = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new java.util.NoSuchElementException();
+                }
+
+                int element = array[index];
+                count++;
+
+                if (count == 3) {
+                    count = 0;
+                    index++;
+                }
+
+                return element;
+            }
+        };
     }
 
     public static Iterator<Integer> intArrayFiveTimesIterator(int[] array) {
-        throw new UnsupportedOperationException();
-        }
+        return new Iterator<Integer>() {
+            private int index = 0;
+            private int count = 0;
 
-    public static Iterable<String> table(String[] columns, int[] rows){
-        throw new UnsupportedOperationException();
+            @Override
+            public boolean hasNext() {
+                return index < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new java.util.NoSuchElementException();
+                }
+
+                int element = array[index];
+                count++;
+
+                if (count == 5) {
+                    count = 0;
+                    index++;
+                }
+
+                return element;
+            }
+        };
     }
 
+    public static Iterable<String> table(String[] columns, int[] rows) {
+        return new Iterable<String>() {
+            @Override
+            public Iterator<String> iterator() {
+                return new Iterator<String>() {
+                    private int columnIndex = 0;
+                    private int rowIndex = 0;
 
+                    @Override
+                    public boolean hasNext() {
+                        return rowIndex < rows.length && columnIndex < columns.length;
+                    }
 
+                    @Override
+                    public String next() {
+                        if (!hasNext()) {
+                            throw new java.util.NoSuchElementException();
+                        }
+
+                        String cell = columns[columnIndex] + rows[rowIndex];
+
+                        rowIndex++;
+                        if (rowIndex >= rows.length) {
+                            rowIndex = 0;
+                            columnIndex++;
+                        }
+
+                        return cell;
+                    }
+                };
+            }
+        };
+    }
 }
